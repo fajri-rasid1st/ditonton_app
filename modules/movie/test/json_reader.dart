36503aec1b1031ffa2d2293faf84b4c0d16d59c0
Muhammad Fajri Rasid <fajrimuhammad460@gmail.com>
2022-05-16 01:07:14 +0800

    remove unused files

--- modules/movie/test/json_reader.dart
+++ modules/movie/test/json_reader.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 String readJson(String name) {
-  var dir = Directory.current.path;
+  var dir = Directory.current.absolute.path;
 
   if (dir.endsWith('/test')) {
     dir = dir.replaceAll('/test', '');
